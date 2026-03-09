86ef0842e5160298303799499ae0cbed7798a516
qiekn <qiekndev@gmail.com>
2026-03-09 14:15:08 +0800

    1st triangle: setup - instance

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,9 @@ public:
 private:
   GLFWwindow* window_ = nullptr;
 
+  vk::raii::Context context_;
+  vk::raii::Instance instance_ = nullptr;
+
   static constexpr uint32_t kWidth = 1200;
   static constexpr uint32_t kHeight = 900;
 
@@ -26,7 +29,7 @@ private:
     window_ = glfwCreateWindow(kWidth, kHeight, "Vulkan", nullptr, nullptr);
   }
 
-  void InitVulkan() {}
+  void InitVulkan() { CreateInstance(); }
 
   void MainLoop() {
     while (!glfwWindowShouldClose(window_)) {
@@ -37,6 +40,34 @@ private:
   void Cleanup() {
     glfwDestroyWindow(window_);
     glfwTerminate();
+  }
+
+  void CreateInstance() {
+    constexpr vk::ApplicationInfo app_info{
+        .pApplicationName = "Hello Triangle",
+        .applicationVersion = vk::makeApiVersion(0, 1, 0, 0),
+        .pEngineName = "No Engine",
+        .engineVersion = vk::makeApiVersion(0, 1, 0, 0),
+        .apiVersion = vk::ApiVersion14};
+
+    uint32_t glfw_extension_count = 0;
+    auto glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
+
+    auto extension_properties = context_.enumerateInstanceExtensionProperties();
+    for (uint32_t i = 0; i < glfw_extension_count; ++i) {
+      if (std::ranges::none_of(extension_properties,
+                               [ext = glfw_extensions[i]](const auto& prop) {
+                                 return strcmp(prop.extensionName, ext) == 0;
+                               })) {
+        throw std::runtime_error("Required GLFW extension not supported: " +
+                                 std::string(glfw_extensions[i]));
+      }
+    }
+
+    vk::InstanceCreateInfo create_info{.pApplicationInfo = &app_info,
+                                       .enabledExtensionCount = glfw_extension_count,
+                                       .ppEnabledExtensionNames = glfw_extensions};
+    instance_ = vk::raii::Instance(context_, create_info);
   }
 };
 
