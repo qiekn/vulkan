16d4a58a32a438cfdd88cf6a5de269179aa3851f
qiekn <qiekndev@gmail.com>
2026-03-09 10:47:13 +0800

    1st triangle: setup - base code

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,52 @@
-import greeting;
+#include <GLFW/glfw3.h>
+
+import vulkan;
 import std;
 
-int main() {
-  greet("Vulkan");
+class HelloTriangleApplication {
+public:
+  void Run() {
+    InitWindow();
+    InitVulkan();
+    MainLoop();
+    Cleanup();
+  }
 
-  std::string name = "qiekn";
-  std::println("Hello, {}!", name);
+private:
+  GLFWwindow* window_ = nullptr;
+
+  static constexpr uint32_t kWidth = 1200;
+  static constexpr uint32_t kHeight = 900;
+
+  void InitWindow() {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    window_ = glfwCreateWindow(kWidth, kHeight, "Vulkan", nullptr, nullptr);
+  }
+
+  void InitVulkan() {}
+
+  void MainLoop() {
+    while (!glfwWindowShouldClose(window_)) {
+      glfwPollEvents();
+    }
+  }
+
+  void Cleanup() {
+    glfwDestroyWindow(window_);
+    glfwTerminate();
+  }
+};
+
+int main() {
+  try {
+    HelloTriangleApplication app;
+    app.Run();
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  }
 
   return 0;
 }
