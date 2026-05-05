c4dfa45ed8b1a0b7c638a9b489d757aedee73a31
qiekn <qiekndev@gmail.com>
2026-05-05 20:59:39 +0800

    chore(uniform): drop unused qiekn padding field

--- src/main.cpp
+++ src/main.cpp
@@ -48,8 +48,7 @@ struct Vertex {
 };
 
 struct UniformBufferObject {
-  glm::vec2 qiekn; // 8 bytes  [ 8 bytes ]
-  alignas(16)glm::mat4 model;
+  glm::mat4 model;
   glm::mat4 view;
   glm::mat4 proj;
 };
